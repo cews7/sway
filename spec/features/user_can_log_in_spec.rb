497457868c2b9627288836e46712f07c69a616d4
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-08-21 13:59:32 -0600

    add model tests for user to confirm email and username

--- spec/features/user_can_log_in_spec.rb
+++ spec/features/user_can_log_in_spec.rb
@@ -18,8 +18,34 @@ RSpec.describe "as a user" do
       expect(page).to have_content("Welcome!")
     end
   end
-  context "unsuccessful login" do
+  context "unsuccessful login with wrong email" do
     it "I am redirected to login" do
+      visit "/login"
+
+      user = User.create(username: "jon", email: "jon@email.com", password: "123jon")
+
+      within ".card-content" do
+        fill_in "session[email]", with: "jo@email.com"
+        fill_in "session[password]", with: "123jon"
+        click_on "Login!"
+      end
+
+      expect(current_path).to eq(login_path)
+    end
+  end
+  context "unsuccessful login with wrong password" do
+    it "I am redirected to login" do
+      visit "/login"
+
+      user = User.create(username: "jon", email: "jon@email.com", password: "123jon")
+
+      within ".card-content" do
+        fill_in "session[email]", with: "jon@email.com"
+        fill_in "session[password]", with: "13jon"
+        click_on "Login!"
+      end
+
+      expect(current_path).to eq(login_path)
     end
   end
 end
