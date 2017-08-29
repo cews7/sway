853a757b746d14a5c9553b3321772342aebdf690
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2017-08-29 12:04:20 -0600

    add factory for trackers

--- spec/factories/trackers.rb
+++ spec/factories/trackers.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :tracker do
     name Faker::Name.name
-    purpose Faker
-    partner "MyString"
-    user nil
+    purpose Faker::Seinfeld.quote
+    partner Faker::Seinfeld.character
+    user 
   end
 end
